419b5d7e3f6c3cc6e686d0ba15e7219c085f7aee
Sebastian <mrseb10@gmail.com>
2024-09-02 11:00:30 +0200

    Created HTTP Trigger function and tested it with Postman

--- AzureFunctions/HttpTrigger.cs
+++ AzureFunctions/HttpTrigger.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Net;
+﻿using System.Net;
+using AzureFunctions.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace AzureFunctions;
 
@@ -16,17 +17,20 @@ public class HttpTrigger
     }
 
     [Function("HttpTrigger")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "POST")] HttpRequestData req,
         FunctionContext executionContext)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation("C# HTTP Trigger function processed a request.");
 
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        response.WriteString("Welcome to Azure Functions!");
-
-        return response;
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var order = JsonConvert.DeserializeObject<Order>(requestBody);
         
+            // TODO: Save order to CosmosDB and add message to Azure Queue
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            response.WriteString("Order received and is being processed.");
+
+            return response;
     }
 }
